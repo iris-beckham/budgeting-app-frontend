720525c05701279de48b0ecd440041b893e1c89a
Iris Beckham <irisdancer3@gmail.com>
2024-04-01 10:04:30 -0700

    beginning of a navigation bar component

---
+++ components/NavigationBar.jsx
@@ -0,0 +1,13 @@
+
+const NavigationBar = () => {
+    return (
+        <>
+            <div>NavigationBar</div>
+            <a href="/">Home</a>
+            <br />
+            <a href="/edit">New Transaction +</a>
+        </>
+    )
+}
+
+export default NavigationBar
