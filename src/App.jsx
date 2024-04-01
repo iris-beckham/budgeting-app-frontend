5891804ed8f359cf84280bee4bcee0b076310627
Iris Beckham <irisdancer3@gmail.com>
2024-04-01 10:03:08 -0700

    components routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Transactions from '../components/Transactions'
+import TransactionDetails from '../components/TransactionDetails'
+import NavigationBar from '../components/NavigationBar'
+import TransactionForm from '../components/TransactionForm'
+
 
 const App = () => {
-  return <div>Budgeting App</div>
+  return <div>
+    <NavigationBar />
+    <h1>Budgeting App</h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Transactions />} />
+        <Route path='/:id' element={<TransactionDetails />}></Route>
+        <Route path='/edit' element={<TransactionForm />}></Route>
+      </Routes>
+    </Router>
+
+  </div>
 }
 
 export default App
